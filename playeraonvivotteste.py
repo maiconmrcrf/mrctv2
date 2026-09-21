44d722369b05ed50028afdd4df462a5b243be8aa
maiconmrcrf <Maiconmrcrf@gmail.com>
2026-09-20 22:12:22 -0300

    Update COOKIE_FIXO value in playeraonvivotteste.py

--- playeraonvivotteste.py
+++ playeraonvivotteste.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 PORTA = int(os.environ.get("PORT", 10000))
 
 USER_AGENT = "Mozilla/5.0 (Android 15; Mobile; rv:155.0) Gecko/155.0 Firefox/155.0"
-COOKIE_FIXO = "__dtsu=51A017746968408D2A64C9BB119F7C31; _ga=GA1.1.82788829.1788834221; _ga_2T9N2RHEW3=GS2.1.s1789837957$o9$g0$t1789837957$j60$l0$h0"
+COOKIE_FIXO = "bitmovin_analytics_uuid=a07b3c21-c8bc-4692-8761-53ffa4df341f"
 ORIGIN_FIXO = "https://1709.cdnembedcanais.xyz"
 
 # ====== CACHE DE SEGMENTOS TS ======
